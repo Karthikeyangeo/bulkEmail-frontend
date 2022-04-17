a94a23a4bb191ff57f96440fd5e5b160ad13c79b
karthikeyangeo <karthikeyangeo@gmail.com>
2022-04-17 22:55:48 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { MailForm } from './routes/MailForm';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MailForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/routes/MailForm.js
@@ -0,0 +1,149 @@
+import React from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import {useFormik} from  "formik";
+import * as yup from 'yup';
+
+
+
+// This validation will split the string based on the comma and check whether it is valid email address
+const email_array_validation =  yup.array()
+                                    .transform(function(value, originalValue) {
+                                        if (this.isType(value) && value !== null) {
+                                            return value;
+                                        }
+                                        return originalValue ? originalValue.split(/[\s,]+/) : [];
+                                        })
+                                    .of(yup.string().email(({ value }) => `${value} --> not a valid email; `))
+                                    
+
+    // email: yup.string()
+    //     .min(5,"Minimum length of email should be 5 chars")
+    //     .matches(/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i, "Invalid Email Address")
+    //     .required("Mandatory field"),
+const formValidationSchema = yup.object({
+    name: yup.string().required("Mandatory Field"),
+    to:email_array_validation.required("Mandatory Field"),
+    cc:email_array_validation,
+    bcc:email_array_validation,
+    subject:yup.string().required("Mandatory Field").min(5,"Please tell us more 📝"),
+    message:yup.string().required("Mandatory Field").min(5,"Please tell us more 📝")     
+  });
+
+//function to split the string and remove the null and change it to Array
+  function toArray(str){
+    if(str){
+         str = str.split(',').filter(x => x);       //filter(x => x) removes the null value in array 
+         return str;
+        }
+    else return null;
+}
+function MailForm(){
+
+    const{handleSubmit,values,handleBlur,handleChange,errors,touched,resetForm} = useFormik({
+        initialValues :{name:'',to:'',cc:'',bcc:'',subject:'',message:''},
+        validationSchema : formValidationSchema,
+        onSubmit:(newValue)=>{
+            newValue.to = toArray(newValue.to);
+            newValue.cc = toArray(newValue.cc);
+            newValue.bcc = toArray(newValue.bcc);
+            console.log('New User',newValue)
+            
+            resetForm();
+        }
+
+    });
+    
+    const new_style = { width: '25%' };
+    return(
+        <form onSubmit={handleSubmit}>
+            <div className='mailForm'>
+                <div className='mailHeader'>
+                    <h1>Bulk Email </h1>
+                </div>
+
+                <TextField
+                    id="name"
+                    name="name"
+                    value={values.name}
+                    onChange={handleChange}
+                    label="Enter your Name"
+                    variant="outlined"
+                    onBlur={handleBlur}
+                    style={new_style} 
+                    error={errors.name && touched.name}
+                    helperText = {errors.name && touched.name ? errors.name : ""}/>
+                
+                <TextField
+                        id="to"
+                        name="to"
+                        value={values.to}
+                        onChange={handleChange}
+                        label="To "
+                        variant="outlined"
+                        placeholder='Enter emails seperated by comma'
+                        onBlur={handleBlur}
+                        style={new_style} 
+                        error={errors.to && touched.to}
+                        helperText = {errors.to && touched.to ? errors.to : ""}/>
+
+                <div className='emailBox'>
+                    <TextField
+                        id="cc"
+                        name="cc"
+                        value={values.cc}
+                        onChange={handleChange}
+                        label="CC "
+                        variant="outlined"
+                        placeholder='Emails seperated by comma'
+                        onBlur={handleBlur}
+                        style={{width: '35%'} } 
+                        error={errors.cc && touched.cc}
+                        helperText = {errors.cc && touched.cc ? errors.cc : ""}/>
+                    
+                    <TextField
+                        id="bcc"
+                        name="bcc"
+                        value={values.bcc}
+                        onChange={handleChange}
+                        label="BCC"
+                        variant="outlined"
+                        placeholder='Emails seperated by comma'
+                        onBlur={handleBlur}
+                        style={{width: '35%'} }
+                        error={errors.bcc && touched.bcc}
+                        helperText = {errors.bcc && touched.bcc ? errors.bcc : ""}/>
+                </div>
+                <TextField
+                    id="subject"
+                    name="subject"
+                    value={values.subject}
+                    onChange={handleChange}
+                    label="Enter mail subject"
+                    variant="outlined"
+                    onBlur={handleBlur}
+                    style={new_style} 
+                    error={errors.subject && touched.subject}
+                    helperText = {errors.subject && touched.subject ? errors.subject : ""}/>
+                <TextField
+                    id="message"
+                    name="message"
+                    multiline
+                    rows={6}
+                    value={values.message}
+                    onChange={handleChange}
+                    label="Enter  message"
+                    variant="outlined"
+                    onBlur={handleBlur}
+                    style={new_style} 
+                    error={errors.message && touched.message}
+                    helperText = {errors.message && touched.message ? errors.message : ""}/>
+
+                {/* Using button from Material  */}
+                <Button variant="contained" type="submit" style={new_style} className="formButton">Send Bulk Email</Button>
+            </div>
+        </form>
+    )
+}
+
+export {MailForm};
