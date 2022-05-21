184643f96b73a88a38052b7927cc41dd81d99bc1
karthikeyangeo <karthikeyangeo@gmail.com>
2022-05-21 12:54:44 +0530

    route

--- src/logOutSign.js
+++ src/logOutSign.js
@@ -9,7 +9,7 @@ export function LogOutSign(){
         sessionStorage.removeItem("currentUser");
         sessionStorage.removeItem("token");
         history.replace('/users/login')     // preventing routing to anypage using back button after logout
-        // history.push("/users/login");
+        
       };
     return(
         <div className='logOutDiv'>
